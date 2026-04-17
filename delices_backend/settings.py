589669f506236b263615818bfefd9e966cc899f5
abdelkane-dev <abdoulkarimt334.gmailcom>
2026-04-17 10:21:39 +0000

    Fix: Add default media files and S3 storage configuration

--- delices_backend/settings.py
+++ delices_backend/settings.py
@@ -89,3 +89,19 @@ CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:
 # Configuration Whitenoise pour les médias
 WHITENOISE_USE_FINDERS = True
 WHITENOISE_IGNORE_FILES = ['*.html']
+
+# Configuration pour stockage média (optionnel - pour production)
+# Pour une vraie solution production, utilisez AWS S3 ou autre stockage cloud
+try:
+    import boto3
+    from storages.backends.s3boto3 import S3Boto3Storage
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default='')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default='')
+    AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+except ImportError:
+    # Si boto3 n'est pas installé, utilise le stockage local
+    pass
